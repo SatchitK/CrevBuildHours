fc55a340d7a8c602fc61137c41eb153c90806dc8
SatchitK <79611051+SatchitK@users.noreply.github.com>
2023-12-26 14:09:20 -0500

    added a lot

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>sign up</p>"
+    if request.method == 'POST': 
+        email = request.form.get('email')
+        fullName = request.form.get('fullname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(fullName) < 2:
+            flash('Name must greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords did not match', category='error')
+        elif len(password1) < 5:
+            flash('Password length must be greater than 5 characters', category='error')
+        else:
+            #add to database
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
---
+++ website/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Hours(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hours = db.Column(db.Integer)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=True)
+    password = db.Column(db.String(250))
+    fullName = db.Column(db.String(250))
